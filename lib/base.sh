efd05d3a844835cfec706f2c351cc662daf1611a
Mingye Wang <arthur200126@gmail.com>
2015-06-01 12:05:30 +0800

    Lib Checking & transparent source.

--- lib/base.sh
+++ lib/base.sh
@@ -1,7 +1,12 @@
-ABLIBS="|base|"
+#!/bin/bash
+# Not to be run directly.
+# TODO:	Add basic support for bashdb.
+shopt -s expand_aliases extglob
+
+declare -x ABLIBS="|base|" # GLOBAL: ABLIBS='|base[|lib1|lib2]|'
 
 bool(){
-  case $1 in
+	case $1 in
 		0|f|F|false|n|N|no) return 1 ;;
 		1|t|T|true|y|Y|yes) return 0 ;;
 		*) return 1 ;;
@@ -10,33 +15,33 @@ bool(){
 
 abreqexe(){
 	for i; do
-	  which $i > /dev/null || abdie "Executable ‘$i’ not found."
+		which $i > /dev/null || abdie "Executable ‘$i’ not found."
 	done
 }
 
 # So ugly...
 
 abloadpm(){
-        . $AB/$ABMPM/lib/pm.sh
-	export ABLIBS="${ABLIBS}pm|"	
+	. $AB/$ABMPM/lib/pm.sh
+	ABLIBS+="pm|"	
 	echo "Loaded library pm" 1>&2
 }
 
 abloadlib(){
-	if [ "x$1" = "xpm" ] 
+	if [ "$1" = "pm" ] 
 	then
 		abloadpm
-		return
+		return $?
 	fi
 	[ -f $ABBLPREFIX/$1.sh ] || return 1
 	. $ABBLPREFIX/$1.sh
-	export ABLIBS="${ABLIBS}$1|"
+	ABLIBS+="${ABLIBS}$1|"
 	echo "Loaded library $1" 1>&2
 }
 
 abrequire(){
 	for i; do
-		echo $ABLIBS | grep "|$i|" > /dev/null || abloadlib $i || abdie "Library ‘$i’ not found."
+		echo $ABLIBS | grep -q "|$i|" || abloadlib $i || abdie "Library ‘$i’ not found."
 	done
 }
 
@@ -48,25 +53,19 @@ ablog(){
 }
 
 returns() { return $*; }
-
-abcommaprint(){ 
-  local FIRST=true
-  for i; do
-		if $FIRST; then FIRST=false
-		else printf " , "; fi
-	  printf $i
-	done
-}
+abcommaprint(){ local cnt; for i; do echo -n "$i"; abmkcomma; done; }
+abmkcomma(){ ((cnt++)) && echo -n "${ABCOMMA- , }"; }
 
 abdie() {
-  echo -e "\e[1;31mautobuild encountered an error and couldn't continue.\e[0m" 1>&2
-  echo -e "${1-Look at the stacktrace to see what happened.}" 1>&2
-  echo "------------------------------autobuild ${VERSION:-3}------------------------------" 1>&2
-  echo -e "Go to ‘\e[1mhttp://github.com/AOSC-Dev/autobuild3\e[0m’ for more information on this error." 1>&2
-  if [ $AB_DBG ]; then read -p "AUTOBUILD_DEBUG: CONTINUE? (Y/N)" -t 5 AB_DBGRUN || AB_DBGRUN=false
-  bool $AB_DBGRUN && abwarn "Forced AUTOBUILD_DIE continue." && return 0 || abdbg "AUTOBUILD_DIE EXIT - NO_CONTINUE/CONTINUE_TIMEDOUT"
-  fi
-  exit ${2-1}
+	echo -e "\e[1;31mautobuild encountered an error and couldn't continue.\e[0m" 1>&2
+	echo -e "${1-Look at the stacktrace to see what happened.}" 1>&2
+	echo "------------------------------autobuild ${VERSION:-3}------------------------------" 1>&2
+	echo -e "Go to ‘\e[1mhttp://github.com/AOSC-Dev/autobuild3\e[0m’ for more information on this error." 1>&2
+	if ((AB_DBG)); then
+		read -n 1 -p "AUTOBUILD_DEBUG: CONTINUE? (Y/N)" -t 5 AB_DBGRUN || AB_DBGRUN=false
+		bool $AB_DBGRUN && abwarn "Forced AUTOBUILD_DIE continue." && return 0 || abdbg "AUTOBUILD_DIE EXIT - NO_CONTINUE"
+	fi
+	exit ${2-1}
 }
 
 # Should these information be redirected into ablog()?
@@ -75,6 +74,23 @@ aberr(){ echo -e "[\e[31mERROR\e[0m]: \e[1m$*\e[0m" >&2; }
 abinfo(){ echo -e "[\e[96mINFO\e[0m]: \e[1m$*\e[0m" >&2; }
 abdbg(){ echo -e "[\e[32mDEBUG\e[0m]:\e[1m$*\e[0m" >&2; }
 ab_dbg(){ local _ret=$?; [ $AB_DBG ] && abdbg "$@"; return $_ret; }
-# Special Source, looks like stacktrace
-.(){ ab_dbg "Source Code from $1:"; source $* || (echo -e "  \e[31min $*\e[0m"; return 1); ab_dbg "---------------"; }
 recsr(){ for sr in "$@"; do . $sr; done }
+argprint(){ local arg; for arg; do printf "%q " "$i"; done; }
+# Special Source, looks like stacktrace
+.(){
+	ab_dbg "Sourcing from $1:"
+	source "$@"
+	local _ret=$? # CATCH_TRANSPARENT
+	returns $_ret || abwarn ". $(argprint "$@") returned $_ret".
+	ab_dbg "End Of $1."
+	return $_ret
+}
+
+# aosc_lib LIBNAME
+aosc_lib(){
+	if [ "$(basename "$0")" == "$1.sh" ]; then
+		abdie "$1 is a library and shouldn't be executed directly." 42
+	fi
+}
+
+aosc_lib base
