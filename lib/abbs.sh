238d27665dd7796fbeafb3153ccf71069645e6a7
Mingye Wang <arthur200126@gmail.com>
2015-06-12 19:02:39 +0800

    lib: changed temp name scheme.

--- lib/abbs.sh
+++ lib/abbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aosc_lib abbs
 abbscheckspec(){ [ -d $ABBS/repo/$1 ] || abdie "No such package specification $1."; }
-abbstmpf(){ mktemp --suffix=abbs; }
-abbsallocenv(){ mktemp -dp "$ABBSENVPOS" --suffix="$1-$(date -u +%s)"; }
+abbstmpf(){ mktemp --suffix=.abbs; }
+abbsallocenv(){ TEMPDIR="$ABBSENVPOS" mktemp -d; }
