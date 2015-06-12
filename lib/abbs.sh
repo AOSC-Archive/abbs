726fdf3bbceb7927bec51d0abb8bfd9b4cda543a
Mingye Wang <arthur200126@gmail.com>
2015-06-12 17:20:28 +0800

    Use mktemp instead of verbose & slow original version.

--- lib/abbs.sh
+++ lib/abbs.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 aosc_lib abbs
 abbscheckspec(){ [ -d $ABBS/repo/$1 ] || abdie "No such package specification $1."; }
-abbstmpf(){
-	while true
-	do
-		fn=/tmp/abbs-$RANDOM
-		if [ ! -e $fn ]
-		then
-			touch $fn
-			echo $fn
-			return 0
-		fi
-	done
-}
-abbsallocenv(){
-	while true
-	do
-		fn=$ABBSENVPOS/$1-$(date -u +%s)-$RANDOM
-		if [ ! -e $fn ]
-		then
-			mkdir $fn
-			echo $fn
-			return 0
-		fi
-	done
-}
+abbstmpf(){ mktemp --suffix=abbs; }
+abbsallocenv(){ mktemp -dp "$ABBSENVPOS" --suffix="$1-$(date -u +%s)"; }
