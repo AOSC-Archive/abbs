9e05d3ed6ca14e4aba790e15946a7ee23352cd11
Mingye Wang <arthur200126@gmail.com>
2015-06-13 18:35:36 +0800

    abbs: fix TMPDIR

--- lib/abbs.sh
+++ lib/abbs.sh
@@ -2,4 +2,4 @@
 aosc_lib abbs
 abbscheckspec(){ [ -d $ABBS/repo/$1 ] || abdie "No such package specification $1."; }
 abbstmpf(){ mktemp --suffix=.abbs; }
-abbsallocenv(){ TEMPDIR="$ABBSENVPOS" mktemp -d; }
+abbsallocenv(){ TMPDIR="$ABBSENVPOS" mktemp -d; }
