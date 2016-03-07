f57bd170d8b094574b3306e5d00a12e7e76ad473
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2016-03-07 10:25:30 -0500

    libabbs: add and del many things at once!

--- lib/abbs.sh
+++ lib/abbs.sh
@@ -6,6 +6,6 @@ abbsallocenv(){ TMPDIR="$ABBSENVPOS" mktemp -d; }
 
 # A simple stringset impl using dicts
 abbs_set(){ declare -gA "$1"; }
-abbs_set_add(){ declare -n __set="$1"; __set["$2"]=1; }
-abbs_set_del(){ unset "$1[$2]"; }
+abbs_set_add(){ declare -n __set="$1"; local __idx; for __idx in "${@:1}"; do __set["$__idx"]=1; done; }
+abbs_set_del(){ local __a=("${@:1}"); __a=("${a[@]/#/$1\[}"); unset "${a[@]/%/\]}"; }
 abbs_set_arr(){ declare -n __set="$1" __arr="$2"; __arr=("${!__set[@]}"); }
