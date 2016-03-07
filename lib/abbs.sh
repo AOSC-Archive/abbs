4b27c43b12459113e49481907eae432c74d60e26
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2016-03-07 08:46:01 -0500

    libabbs: poor man's set
    Trying to get rid of all those TMPF nonsense.

--- lib/abbs.sh
+++ lib/abbs.sh
@@ -3,3 +3,9 @@ aosc_lib abbs
 abbscheckspec(){ [ -d $ABBS/repo/$1 ] || abdie "No such package specification $1."; }
 abbstmpf(){ mktemp --suffix=.abbs; }
 abbsallocenv(){ TMPDIR="$ABBSENVPOS" mktemp -d; }
+
+# A simple stringset impl using dicts
+abbs_set(){ declare -gA "$1"; }
+abbs_set_add(){ declare -n __set="$1"; __set["$2"]=1; }
+abbs_set_del(){ unset "$1[$2]"; }
+abbs_set_arr(){ declare -n __set="$1" __arr="$2"; __arr=("${!__set[@]}"); }
