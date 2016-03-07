c01ac0b5dd7835f74e236a907476b533a61e7ca4
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2016-03-07 08:30:07 -0500

    lib, env/base: get rid of ab3 copies
    We can define a new abloadlib later:
    
    ```bash
    abloadlib(){
        # try ABBS:lib
    
        # try AB:lib
    }
    readonly -f abloadlib
    ```

--- env/base.sh
+++ env/base.sh
@@ -1,12 +1,13 @@
 : ${ABSET=/etc/autobuild} ${AB=$(<"$ABSET/prefix")}
-: ${ABBSSET=/etc/abbs} ${ABBS=$(<"$ABBSSET/prefix")} ${ABBLPREFIX="$ABBS/lib"}
+: ${ABBSSET=/etc/abbs} ${ABBS=$(<"$ABBSSET/prefix")} ${ABBLPREFIX="$AB/lib"}
 PATH="$ABBS/libexec:$PATH"
 export ABSET AB ABBSSET ABBS ABBLPREFIX PATH ABSET
 
 ((ABBSENV_VARONLY)) && return
 # Load the base lib
-. $ABBS/lib/base.sh
-abloadlib abbs
+. "$AB"/lib/base.sh
+# HACK!
+. "$ABBS"/lib/abbs.sh && ABLIBS+='abbs|'
 
 recsr $ABBS/etc/abbs/*
 
