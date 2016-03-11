7b6ea7e5a9b0969d9e14bdf3e97a8d5e366462df
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2016-03-11 08:40:51 -0500

    trunk

--- lib/abbs.sh
+++ lib/abbs.sh
@@ -5,3 +5,12 @@ abbscheckspec(){ [ -d $ABBS/repo/$1 ] || abdie "No such package specification $1
 abbstmpf(){ mktemp --suffix=.abbs; }
 abbsallocenv(){ TMPDIR="$ABBSENVPOS" mktemp -d; }
 
+# HACK: awful override function
+_abloadlib=$(declare -f abloadlib)
+eval "${_abloadlib/abloadlib/_abloadlib}"
+readonly -f _abloadlib
+unset _abloadlib
+abloadlib(){
+	ABBLPREFIX=$ABBS/lib _abloadlib "$@" ||
+	ABBSPREFIX=$AB/lib   _abloadlib "$@"
+}
